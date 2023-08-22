485769dd2bf1aaee46e35c5fb8a6e6cbcfba335d
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2023-08-22 20:25:59 +0300

    Add `Program.cs`

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+class Program {
+
+    static async Task Main(string[] args) {
+        await Interactive.RunAsync();
+    }
+
+}
