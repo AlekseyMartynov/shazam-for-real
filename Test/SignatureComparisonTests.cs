8e6310a62886d82ee728be4a3348fce9ab0f90ef
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2023-09-11 12:04:00 +0300

    `sampleCount` difference puzzle (9dd7b3680) solved

--- Test/SignatureComparisonTests.cs
+++ Test/SignatureComparisonTests.cs
@@ -16,6 +16,7 @@ namespace Project.Test {
             CreateFromWaveFile(
                 Path.Combine(TestHelper.DATA_DIR, "test2.wav"),
                 out var mySampleCount,
+                out var myRemainingSampleCount,
                 out var myPeaks
             );
 
@@ -26,12 +27,17 @@ namespace Project.Test {
                 out var refPeaks
             );
 
-            Assert.Equal(refSampleCount, mySampleCount);
+            // Official signature has more peaks
+            // Most of them are at edges < RADIUS_TIME
 
-            var anchorBin = 38;
-            var myStripe = myPeaks.FindOne(anchorBin).StripeIndex;
-            var refStripe = refPeaks.FindOne(anchorBin).StripeIndex;
-            var refStripeOffset = refStripe - myStripe;
+            // Possible explanation for padding in official signature
+            // start - first chunk completes the FFT window so it is immediately ready for analysis
+            // end - to absorb remaining samples? for symmetry?
+
+            var refSampleCountPadding = Analysis.WINDOW_SIZE - Analysis.CHUNK_SIZE;
+            var refStripeOffset = refSampleCountPadding / Analysis.CHUNK_SIZE;
+
+            Assert.Equal(refSampleCount - 2 * refSampleCountPadding, mySampleCount + myRemainingSampleCount);
 
             var hitCount = 0;
             var missCount = 0;
@@ -61,7 +67,7 @@ namespace Project.Test {
             Assert.True(Math.Abs(magnFitIntercept) < 10);
         }
 
-        static void CreateFromWaveFile(string path, out int sampleCount, out IReadOnlyList<PeakInfo> peaks) {
+        static void CreateFromWaveFile(string path, out int sampleCount, out int remainingSampleCount, out IReadOnlyList<PeakInfo> peaks) {
             var analysis = new Analysis();
             var finder = new PeakFinder(analysis);
 
@@ -69,7 +75,6 @@ namespace Project.Test {
             var sampleProvider = wave.ToSampleProvider();
 
             var chunk = new float[Analysis.CHUNK_SIZE];
-            var remainingSampleCount = 0;
 
             while(true) {
                 var readCount = sampleProvider.Read(chunk, 0, chunk.Length);
@@ -82,7 +87,7 @@ namespace Project.Test {
                 analysis.AddChunk(chunk);
             }
 
-            var sigBytes = Sig.Write(Analysis.SAMPLE_RATE, analysis.ProcessedSamples + remainingSampleCount, finder);
+            var sigBytes = Sig.Write(Analysis.SAMPLE_RATE, analysis.ProcessedSamples, finder);
             LoadBinary(sigBytes, out sampleCount, out peaks);
         }
 
