6a629b5019521e1e44e7d8172c3839f936cd8f5e
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2024-10-05 16:46:35 +0300

    Add `libssl-dev:arm64`

--- .github/workflows/install-ubuntu-arm64.sh
+++ .github/workflows/install-ubuntu-arm64.sh
@@ -10,6 +10,6 @@ sed -i "s|CODENAME|$CODENAME|g" /etc/apt/sources.list.d/ubuntu.sources
 dpkg --add-architecture arm64
 
 apt-get update
-apt-get install -y llvm zlib1g-dev:arm64 binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu
+apt-get install -y llvm libssl-dev:arm64 zlib1g-dev:arm64 binutils-aarch64-linux-gnu gcc-aarch64-linux-gnu
 
 apt-get install qemu-user
