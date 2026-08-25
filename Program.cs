4421f7b7ac7b35e273e57f2f9d7a940d38b9b421
AlekseyMartynov <AlekseyMartynov@users.noreply.github.com>
2026-08-25 20:15:13 +0300

    xunit.v3
    Closes #62
    Closes #67
    Closes #68

--- Program.cs
+++ Program.cs
@@ -7,12 +7,22 @@ namespace Project;
 
 class Program {
 
-    static async Task Main(string[] args) {
+    static async Task<int> Main(string[] args) {
+#if DEBUG
+        if(IsTestRun(args)) {
+            return await Xunit.MicrosoftTestingPlatform.TestPlatformTestFramework.RunAsync(args, SelfRegisteredExtensions.AddSelfRegisteredExtensions);
+        }
+#endif
         if(args.Length > 0) {
             await TagFile.RunAsync(args);
         } else {
             await Interactive.RunAsync();
         }
+        return default;
     }
 
+    static bool IsTestRun(string[] args) {
+        // https://github.com/microsoft/testfx/tree/main/docs/mstest-runner-protocol
+        return args.Contains("--server");
+    }
 }
--- Test/PeakDensityResearch.cs
+++ Test/PeakDensityResearch.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Project.Test;
 
